b527fb51aaf4d2a2da7aee1cc22ad2a9903bf113
Andrey Tumilovich <Andrejt0786@gmail.com>
2022-07-02 11:05:30 +0300

    With tests

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,28 +1,46 @@
-import java.awt.desktop.PrintFilesEvent
-import java.sql.DriverManager.println
+
+
+import java.util.*
+
 
 fun main() {
     val commentToWall = CommentToWall(5, true, true, false, false)
 
+
     val post = Post(
-        1, 1, 2, 1, 356, "Super",
+        5, 1, 2, 1, 356, "Super",
         3, 1, false, commentToWall
     )
     val post2 = Post(
-        1, 1, 2, 1, 35, "Super",
+        1, 3, 2, 1, 35, "Super",
         3, 1, false, commentToWall
     )
-    WallService.add(post2)
-    WallService.add(post)
+    val post3 = Post(
+        1, 55, 2, 1, 35, "Super",
+        3, 1, false, commentToWall
+    )
+    val post4 = Post(
+        6, 9, 2, 1, 35, "Super",
+        3, 1, false, commentToWall
+    )
+    val postTest = Post(
+        3, 222, 2, 1, 35, "Super",
+        3, 1, false, commentToWall
+    )
 
+    // Убрано для проверки тестов
+    /*println(WallService.add(post2))
+    println(WallService.add(post))
+    println(WallService.add(post3))
+    println(WallService.add(post4))
 
+    print(WallService.update(postTest))*/
 
 }
 
 
-
 data class Post(
-    val id: Int,
+    var id: Int,
     val ownerId: Int,
     val fromId: Int,
     val createBy: Int,
@@ -42,15 +60,46 @@ data class CommentToWall(
     val canOpen: Boolean
 )
 
-object WallService {
+class WallService { // object заменен на class  для проверки тестов
     var posts = emptyArray<Post>()
-    fun add(post: Post) {
-        posts += post
-        
-        println(posts.last())
+
+    fun add(post: Post): Post {
+        if (posts?.isEmpty() ?: true) {
+            posts += post
+        } else {
+            val (id) = posts.last()
+            val idSpecial = post.copy(id = id + 1)
+            posts += idSpecial
+        }
+        return posts.last()
     }
 
-}
+
+    fun update(post: Post): Boolean {
+        var updateTrue = false
+        val (id) = post
+        for ((index, post) in posts.withIndex()) {
+            if (post.id == id) {
+                updateTrue = true
+                posts[index] = post.copy(
+                    ownerId = post.ownerId,
+                    fromId = post.fromId,
+                    createBy = post.createBy,
+                    date = post.date,
+                    text = post.text,
+                    replyOwnerId = post.replyOwnerId,
+                    replyPostId = post.replyPostId,
+                    friendOnly = post.friendOnly,
+                    commentToWall = post.commentToWall
+                )
+            }
+        }
+        return updateTrue
+    }
+
+
+    }
+
 
 
 
---
+++ src/test/kotlin/WallServiceTest1.kt
@@ -0,0 +1,67 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceTest {
+
+    @Test
+    fun add() {
+        val commentToWall = CommentToWall(5, true, true, false, false)
+        val service = WallService()
+        service.add(Post(
+                1, 9, 2, 1, 35, "Super",
+            3, 1, false, commentToWall
+        ))
+        val result = service.add(
+            Post(
+                6, 9, 2, 1, 35, "Super",
+                3, 1, false, commentToWall
+            )
+        )
+        val(id)=result
+        assertNotEquals(0,id)
+
+
+    }
+
+    @Test
+    fun updateFalse() {
+        val commentToWall = CommentToWall(5, true, true, false, false)
+        val service = WallService()
+        service.add(
+            Post(
+                6, 9, 2, 1, 35, "Super",
+                3, 1, false, commentToWall
+            )
+        )
+        val post = Post(
+            1, 9, 2, 1, 35, "Super",
+            3, 1, false, commentToWall
+        )
+        val result = service.update(post)
+        assertEquals(false, result)
+    }
+
+
+    @Test
+    fun updateTrue() {
+        val commentToWall = CommentToWall(5, true, true, false, false)
+        val service = WallService()
+        service.add(
+            Post(
+                6, 9, 2, 1, 35, "Super",
+                3, 1, false, commentToWall
+            )
+        )
+        val post = Post(
+            1, 9, 2, 1, 35, "Super",
+            3, 1, false, commentToWall
+        )
+        val result = service.update(post)
+        assertEquals(false, result)
+
+    }
+
+
+}
+
